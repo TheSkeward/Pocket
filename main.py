4099a0c2282ea812933f13844a31c3692b268b4a
Jason Tan <10.1.19.15.14.tan@gmail.com>
2016-03-08 19:44:07 -0500

    Name change to Pocket

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ ignore_list = []
 @client.event
 async def on_ready():
     logging.info("Logged in as " + client.user.name + " (" + client.user.id + ")")
-    ignore_list.append(client.user.id)              # Ignore self, or else Muse would respond to himself
+    ignore_list.append(client.user.id)              # Ignore self, or else Pocket would respond to himself
 
 @client.event
 async def on_message(message: discord.Message):
@@ -24,18 +24,18 @@ async def on_message(message: discord.Message):
         addressed = is_addressed(message)
         content = message.content
         if addressed:
-            content = content[5:].strip()           # Remove the prefix ("muse," or "Muse:"), which is 5 chars long
+            content = content[7:].strip()           # Remove the prefix ("pocket," or "Pocket:"), which is 5 chars long
 
         await client.send_message(message.channel, parse(message, content, addressed))
 
 
 def is_addressed(message: discord.Message):
     """
-    Determines if muse was addressed, ie "Muse, inspire me" or "muse: give me a suggestion"
+    Determines if Pocket was addressed, ie "Pocket, inspire me" or "pocket: give me a suggestion"
     If he was addressed, returns the rest of the message. If not, then returns None
     """
     prefix = message.content.split()[0].lower()
-    if prefix == "muse," or prefix == "muse:":
+    if prefix == "pocket," or prefix == "pocket:":
         return True
     return False
 
